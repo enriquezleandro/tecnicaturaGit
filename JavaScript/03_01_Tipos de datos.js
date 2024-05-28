783ca463e953cf0858f32f37aa561fc3461abb3a
Leandro <enriquezleandro@gmail.com>
2024-05-28 00:20:40 -0300

    Clase 04

--- JavaScript/03_01_Tipos de datos.js
+++ JavaScript/03_01_Tipos de datos.js
@@ -1,12 +1,33 @@
+//TIPOS DE DATOS
+//Tipo cadena
 var nombre = "Javier";
 console.log(nombre);
 
+//Tipo numerico
 var numero = 2027;
 console.log(numero);
 
+//Tipo objetos
 var objeto = {
-nombre: "Javier",
-apellido: "Milei",
-telefono: "2027"
-}
+  nombre: "Javier",
+  apellido: "Milei",
+  telefono: "2027",
+};
 console.log(objeto);
+
+//Tipo funcion
+function miFuncion() {}
+console.log(miFuncion);
+
+//Tipo symbol
+var simbolo = Symbol("mi simbolo");
+console.log(simbolo);
+
+//Tipo clase
+class Persona {
+  constructor(nombre, apellido) {
+    this.nombre = "Leandro";
+    this.apellido = "Enriquez";
+  }
+}
+console.log(typeof Persona)
---
+++ JavaScript/05_01_Tipos de datos.js
@@ -0,0 +1,16 @@
+//Tipo de dato undefined
+let x;
+console.log(x);
+
+//null: ausencia de valor, null no es un tipo de dato, pero su origen es de tipo object
+let y = null;
+console.log(typeof y)
+
+//Tipo array y empy string
+let autos = ['Citroen', 'Ford', 'Chevrolet'];
+console.log(autos);
+console.log(typeof autos);
+
+//Tipo cadena vacia
+var z = '';
+console.log(z);
