7b6fdb1ac115222ea2651ebdfaebb3e2daca54ff
Leandro <enriquezleandro@gmail.com>
2024-05-28 00:21:15 -0300

    Clase 04 Js

--- JavaScript/05_01_Tipos de datos.js
+++ JavaScript/05_01_Tipos de datos.js
@@ -4,13 +4,13 @@ console.log(x);
 
 //null: ausencia de valor, null no es un tipo de dato, pero su origen es de tipo object
 let y = null;
-console.log(typeof y)
+console.log(typeof y);
 
 //Tipo array y empy string
-let autos = ['Citroen', 'Ford', 'Chevrolet'];
+let autos = ["Citroen", "Ford", "Chevrolet"];
 console.log(autos);
 console.log(typeof autos);
 
 //Tipo cadena vacia
-var z = '';
+var z = "";
 console.log(z);
